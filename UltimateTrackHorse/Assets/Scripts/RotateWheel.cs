4d18c6ab597c94231969e5d1440f6df2bb13a608
SrockNiper <113103066+SrockNiper@users.noreply.github.com>
2026-02-23 17:31:58 +0100

    Update RotateWheel.cs

--- UltimateTrackHorse/Assets/Scripts/RotateWheel.cs
+++ UltimateTrackHorse/Assets/Scripts/RotateWheel.cs
@@ -12,22 +12,12 @@ public class RotateWheel : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Získání vstupu (-1 až 1 podle kláves A/D)
+      
         horizontalInput = Input.GetAxis("Horizontal");
-
-        // Vypoèítáme cílový úhel (napø. -1 * 45 = -45 stupòù)
         float targetAngle = horizontalInput * maxSteerAngle;
-
-        // Plynulý pøesun aktuálního úhlu smìrem k cílovému úhlu
         rotationAngle = Mathf.MoveTowards(rotationAngle, targetAngle, rotationSpeed * Time.deltaTime);
-
-        // Aplikování rotace na objekt. 
-        // POZNÁMKA: Pokud se ti kolo toèí ve špatné ose, uprav Vector3.
-        // (0, rotationAngle, 0) otáèí kolo do stran jako u auta (osa Y).
-        // (0, 0, -rotationAngle) by otáèelo kolem jako volantem (osa Z).
         transform.localEulerAngles = new Vector3(0, rotationAngle, 0);
 
     }
