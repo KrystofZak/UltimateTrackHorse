fd1e511f2cfc786d3d6c0ef6a5f29a67dd694373
Tern04 <113103166+Tern04@users.noreply.github.com>
2026-02-24 14:06:40 +0100

    TileData and Cell script refractoring

--- UltimateTrackHorse/Assets/Scripts/MapGeneration/Cell.cs
+++ UltimateTrackHorse/Assets/Scripts/MapGeneration/Cell.cs
@@ -1,23 +1,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cell
+namespace MapGeneration
 {
-    public Vector2Int gridPosition; // Position of the cell in the grid
-    public bool isCollapsed;        // Indicates whether the cell has been collapsed to a single tile or not
-    public List<TileData> availableTiles; // List of possible tiles that can still be placed in this cell based on the WFC constraints
-    public TileData collapsedTile;  // Final tile that was chosen
-
-    // Constructor of the cell, initializes it with all possible tiles at the beginning
-    public Cell(Vector2Int pos, List<TileData> allTiles)
+    public class Cell
     {
-        gridPosition = pos;
-        isCollapsed = false;
-        
-        // All tiles are initialy available
-        availableTiles = new List<TileData>(allTiles);
-    }
+        public Vector2Int GridPosition; // Position of the cell in the grid
+        public bool IsCollapsed; // Indicates whether the cell has been collapsed to a single tile or not
 
-    // Entropy - number of possible tiles that can be placed
-    public int Entropy => availableTiles.Count; 
+        public readonly List<TileData>
+            AvailableTiles; // List of possible tiles that can still be placed in this cell based on the WFC constraints
+
+        public TileData CollapsedTile; // Final tile that was chosen
+
+        // Constructor of the cell, initializes it with all possible tiles at the beginning
+        public Cell(Vector2Int pos, List<TileData> allTiles)
+        {
+            GridPosition = pos;
+            IsCollapsed = false;
+
+            // All tiles are initially available
+            AvailableTiles = new List<TileData>(allTiles);
+        }
+
+        // Entropy - a number of possible tiles that can be placed
+        public int Entropy => AvailableTiles.Count;
+    }
 }
